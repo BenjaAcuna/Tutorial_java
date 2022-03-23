1c37c6ccfca23fb70dd39f811499659335689187
BenjaAcuna <benjaminacuna06@gmail.com>
2022-03-23 19:14:56 -0300

    Tutorial 23

---
+++ Metodos_y_Objetos/principal/Objeto.java
@@ -0,0 +1,29 @@
+package principal;
+
+public class Objeto
+{
+   int width;
+   int height;
+   int rad;
+
+   String Nombre;
+
+   public Objeto()
+   {
+        this.width = 100;
+        this.height = 100;
+        this.rad = 100;
+   }
+
+     public void cambiarTamano(int nuevoTamano)
+     {
+        this.height= nuevoTamano;
+        this.width= nuevoTamano;
+        
+     }
+     public void imprimirTamano()
+     {
+         System.out.println("Ancho: "+this.width);
+         System.out.println("Alto: "+this.height);
+     }
+}
---
+++ Metodos_y_Objetos/principal/principal.java
@@ -0,0 +1,19 @@
+package principal;
+
+public class principal {
+ 
+    
+    public static void main(String[] args){
+    
+    Objeto a = new Objeto();
+    Objeto b = new Objeto();
+    
+    a.cambiarTamano(50);
+
+    b.cambiarTamano(70);
+
+    a.imprimirTamano();
+    b.imprimirTamano();
+    
+  }
+}
