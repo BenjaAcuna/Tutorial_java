7e641c6cce7a53c01fc645d6ba79404d371c84ed
BenjaAcuna <benjaminacuna06@gmail.com>
2022-03-24 18:25:14 -0300

    Cronometro

---
+++ Cronometro/Main.java
@@ -0,0 +1,29 @@
+class Main{
+
+public static void main(String[] args){
+
+int minutos = 0;
+int segundos = 0;
+for(minutos = 0;minutos < 60;minutos++)
+{
+
+ for (segundos=0;segundos<60;segundos++)
+ {
+     
+   System.out.println(minutos+":"+segundos);
+   delaySegundo();
+
+     }
+   }
+ }
+
+private static void delaySegundo()
+{
+
+    try{
+        Thread.sleep(1000);
+    }catch(InterruptedException e){}
+
+
+ }
+}
