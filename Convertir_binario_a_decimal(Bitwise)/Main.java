a0a2f28c3fd2f0421ad9d267646bb2f464c7ec01
BenjaAcuna <benjaminacuna06@gmail.com>
2022-03-29 12:49:09 -0300

    Datos

---
+++ Convertir_binario_a_decimal(Bitwise)/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+         System.out.println(toInt("111"));
+    }
+
+    public static int toInt(String bin){
+
+        int result = 0;
+        char binario[] = bin.toCharArray();
+        for(int i = 0; i < binario.length; i++,result <<=1 ){
+            result |= (binario[i] == '0')?0:1;
+        }
+        result >>= 1;
+        return result;
+    }
+}
